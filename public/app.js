f7c356c9f361292d213d067280bdc23e68ac9133
zohebibrahim <zohebibrahimm@gmail.com>
2026-06-04 13:22:52 +0200

    commt 6

--- public/app.js
+++ public/app.js
@@ -17,6 +17,26 @@ document.addEventListener('DOMContentLoaded', () => {
   const statusBadge = document.getElementById('statusBadge')
   const componentTree = document.getElementById('componentTree')
 
-  // inloggegevens voor de test
+  // inloggegevens voor de login page
   const GELDIGE_GEBRUIKERSNAAM = 'zoheb'
   const GELDIG_WACHTWOORD = 'zoheb'
+
+  // als de inloggegevens kloppen dan word de vadilator pagina getoond
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    const usernameInput = document.getElementById('username').value.trim()
+    const passwordInput = document.getElementById('password').value
+
+    if (usernameInput === GELDIGE_GEBRUIKERSNAAM && passwordInput === GELDIG_WACHTWOORD) {
+      loginPage.style.opacity = '0'
+      loginPage.style.transform = 'scale(0.98)'
+      
+      setTimeout(() => {
+        loginPage.style.display = 'none'
+        appContainer.classList.remove('app-hidden')
+      }, 400)
+    } else {
+      loginError.textContent = 'Onjuiste gebruikersnaam of wachtwoord.'
+    }
+  })  
